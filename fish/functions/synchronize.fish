a103372f040d48c8a5998c29b2b92a36b962fbf6
bigi.pigna <alessandro.biagiotti@studenti.unimi.it>
2024-12-31 10:44:11 +0100

    Added a simple function to upload and download the contents of the Obsidian Vault

---
+++ fish/functions/synchronize.fish
@@ -0,0 +1,34 @@
+function synchronize
+
+	# Set the function's flags
+	set --local options 'h/help' 'u/upload' 'd/download'
+	argparse $options -- $argv
+	
+	if set --query _flag_help
+		printf "Usage: synchronize [-h -u -d]\n\n"
+		printf "Options:\n"
+		printf "  -h/--help\t\tPrints help and exits\n"
+		printf "  -u/--upload\t\tUploads the modifications done to the repository to Github\n"
+		printf "  -d/--download\t\tDownloads locally the contents of the Github repository\n"
+		return
+	end
+
+	set --local vault "/home/heart-of-gold/obsidian-vault/"
+
+	if [ (pwd) != $vault ]
+		echo "Moving to the vault -- $vault"
+		cd $vault
+	end
+
+	if set --query _flag_upload
+		# Pushing to master
+		git add .
+		git commit -m "Synchronization for machine: $hostname"
+		git push
+	end
+
+	if set --query _flag_download
+		# Pulling from source
+		git pull
+	end
+end
