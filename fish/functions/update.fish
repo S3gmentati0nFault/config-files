1f1316a8e22af7dea86bd1ec3d415517c2a98c59
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2025-02-04 23:16:28 +0100

    Changed the update function to work with symlinks

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,29 +1,39 @@
 function update
-	set --local options 'h/help' 'r/repo' 'l/local'
-	argparse $options -- $argv
 
-	if set --query _flag_help
-		printf "Usage: synchronize [-h -r -l]\n\n"
-		printf "Options:\n"
-		printf "  -h/--help\t\tPrints help and exits\n"
-		printf "  -r/--repo\t\tMoves any modification done to the configurations into the
-			Github repository\n"
-		printf "  -l/--local\t\tMoves any modification done to the configurations into
-			the local configuration path\n"
-		return
-	end
+	# Variables
+	set --local home /home/heartofgold
+	set --local wezterm $home/.config/wezterm
+	set --local fish $home/.config/fish/functions
+	set --local backup $home/.config/dotbackup
+	set --local dotfiles $home/Projects/config-files
 
-	set --local current (pwd)
-	set --local user (id -un)
-	set --local home "/home/$user"
-	
-	if set --query _flag_repo
-		cp -rf "$home/.config/fish/functions" "$home/Projects/config-files/fish/"
-		cp -rf "$home/.config/nvim/" "$home/Projects/config-files/"
-	end
+	# Create the old dotfiles dir
+	echo "Generating the DOTBACKUP folder"
+	mkdir -p $backup
+	echo "...done\n\n\n"
 
-	if set --query _flag_local
-		cp -rf "$home/Projects/config-files/fish/functions" "$home/.config/fish/"
-		cp -rf "$home/Projects/config-files/nvim/" "$home/.config/"
+	# Copy all of the functions from the original fish folder to the backup
+	for file in $fish/*
+		echo "Moving all of the fish functions to the backups"
+		set --local base (basename $file)
+		echo "$file --> $backup/$base"
+		mv -f "$file" "$backup/$base"
 	end
+	echo "...done"
+
+	# Copy the old configuration of wezterm to the backup folder
+	echo "Moving the wezterm configuration to backup"
+	mv -f "$wezterm/wezterm.lua" "$backup/wezterm.lua"
+	echo "...done"
+
+	# Symlink every file in the fish functions directory and add the symlink to the wezterm
+	# configuration file
+	for file in $dotfiles/fish/functions/*
+		echo "Moving all the fish functions from the dotfiles dir to the config function"
+		set --local base (basename $file)
+		echo "$file --> $fish/$base"
+		ln -s $file $fish/$base
+	end
+	ln -s $dotfiles/wezterm/wezterm.lua $wezterm/wezterm.lua
+	echo "...done"
 end
