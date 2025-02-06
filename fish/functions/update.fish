6e0ba0f1b10e1f50ab92518ead50d82d928824ad
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2025-02-06 21:12:01 +0100

    Remove stupid messages from previous merge attempt

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,19 +1,11 @@
 function update
 
 	# Variables
-<<<<<<< HEAD
 	set --local home /home/heartofgold
-=======
-	set --local home /home/heart-of-gold
->>>>>>> 73777506182a46cf1f78ed2e91f7cbedbbfaf17c
 	set --local wezterm $home/.config/wezterm
 	set --local fish $home/.config/fish/functions
 	set --local backup $home/.config/dotbackup
 	set --local dotfiles $home/Projects/config-files
-<<<<<<< HEAD
-=======
-	set --local wm $home/.config/i3
->>>>>>> 73777506182a46cf1f78ed2e91f7cbedbbfaf17c
 
 	# Create the old dotfiles dir
 	echo "Generating the DOTBACKUP folder"
@@ -34,18 +26,8 @@ function update
 	mv -f "$wezterm/wezterm.lua" "$backup/wezterm.lua"
 	echo "...done"
 
-<<<<<<< HEAD
 	# Symlink every file in the fish functions directory and add the symlink to the wezterm
 	# configuration file
-=======
-	# Copy the old configuration of i3 to the backup folder
-	echo "Moving the i3 configuration to backup"
-	mv -f "$wm/config" "$backup/i3/config"
-	echo "...done"
-
-	# Symlink every file in the fish functions directory and add the symlink to the wezterm and
-	# i3 configuration file
->>>>>>> 73777506182a46cf1f78ed2e91f7cbedbbfaf17c
 	for file in $dotfiles/fish/functions/*
 		echo "Moving all the fish functions from the dotfiles dir to the config function"
 		set --local base (basename $file)
@@ -53,9 +35,5 @@ function update
 		ln -s $file $fish/$base
 	end
 	ln -s $dotfiles/wezterm.lua $wezterm/wezterm.lua
-<<<<<<< HEAD
-=======
-	ln -s $dotfiles/i3/config $wm/config
->>>>>>> 73777506182a46cf1f78ed2e91f7cbedbbfaf17c
 	echo "...done"
 end
