d9f38b131fbc16f0c357a9b2d41a4d44b6fe8017
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2025-02-14 10:19:12 +0100

    Changed the wezterm configuration

--- wezterm.lua
+++ wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 --config.color_scheme = "AdventureTime"
-config.font = wezterm.font("FreeMono", { weight = "Bold" })
-config.font_size = 14.0
+config.font = wezterm.font("Agave Nerd Font Mono", { weight = "Regular" })
+config.font_size = 12.0
 config.line_height = 1.5
 
 config.colors = {
