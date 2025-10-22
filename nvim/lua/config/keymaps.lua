1453a32272ac3ea2df1c2018e33556f568d69ef7
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2025-10-22 17:48:02 +0200

    Added the keymaps for the terminal

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>wt", [[<cmd>vsplit | term<cr>A]], { desc = "Open terminal in vertical split" })
+vim.keymap.set("n", "<leader>wf", [[<cmd>split | term<cr>A]], { desc = "Open terminal in horizontal split" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
